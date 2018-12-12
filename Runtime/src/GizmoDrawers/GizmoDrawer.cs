191a33cde427a8b8f15aaa8d3e64876de90957bf
Walter Blacke <theoriginalsine@gmail.com>
2018-12-11 21:54:50 -0500

    Organization and Unity setup

---
+++ Runtime/src/GizmoDrawers/GizmoDrawer.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+namespace SINeDUSTRIES.Unity
+{
+  /// <summary>
+  /// <see cref="Component"/> which draws <see cref="Gizmos"/> for other <see cref="Component"/>;
+  /// </summary>
+  /// <typeparam name="TComponent">Type of component to draw for;</typeparam>
+  [ExecuteInEditMode]
+  public abstract class GizmoDrawer<TComponent> : MonoBehaviour
+    where TComponent : Component
+  {
+    // TODO: gizmo drawer for ScrollRect
+
+    #region Protected, Methods
+
+    /// <summary>
+    /// <see cref="MonoBehaviour"/> message;
+    /// </summary>
+    virtual protected void OnDrawGizmos()
+    {
+      Color old = Gizmos.color; // cache current color
+
+      Gizmos.color = colorMain; // set color
+
+      if // if
+      (
+        this._component != null && // component is not null
+        this.isActiveAndEnabled
+      )
+      {
+        onDrawGizmosComponent(); // draw the gizmo for the component
+      }
+
+      Gizmos.color = old; // restore
+    }
+
+    /// <summary>
+    /// Draw the gizmo for the attatched component;
+    /// </summary>
+    abstract protected void onDrawGizmosComponent();
+
+    #endregion
+
+    #region Protected, Properties
+
+    /// <summary>
+    /// The color of thhis Gizmo;
+    /// </summary>
+    protected Color colorMain
+    => _colorMain;
+
+    #endregion
+
+    #region Private, Fields
+
+    /// <summary>
+    /// The component drawing a Gizmo for;
+    /// </summary>
+    [SerializeField] protected TComponent _component;
+
+    #endregion
+
+    #region Private, Fields
+
+    /// <summary>
+    /// Editable Main Color;
+    /// </summary>
+    [SerializeField] Color _colorMain;
+
+    #endregion
+
+    #region Lifecycle
+
+    /// <summary>
+    /// <see cref="MonoBehaviour"/> message;
+    /// </summary>
+    virtual protected void Reset()
+    {
+      _component = GetComponent<TComponent>(); // get the component
+
+      _colorMain = resetColorDefaultGet(); // get 
+    }
+
+    /// <summary>
+    /// Reset the color;
+    /// Default color drawn by gizmos of this type;
+    /// </summary>
+    /// <notes>
+    /// Merely a preference by ~me~;
+    /// </notes>
+    abstract protected Color resetColorDefaultGet();
+
+    #endregion
+  }
+}
---
+++ Runtime/src/GizmoDrawers/GizmoDrawerRectCorners.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace SINeDUSTRIES.Unity
+{
+  /// <summary>
+  /// GizmoDrawer for something with <see cref="RectCorners"/>;
+  /// </summary>
+  public abstract class GizmoDrawerRectCorners<TComponent> : GizmoDrawer<TComponent>
+    where TComponent : Component
+  {
+    #region Protected, Method
+
+    /// <summary>
+    /// <see cref="GizmoDrawer{TComponent}.onDrawGizmosComponent"/>;
+    /// </summary>
+    override protected void onDrawGizmosComponent()
+    {
+      _corners.GizmoDraw(); // draw the gizmo for the corners
+    }
+
+    /// <summary>
+    /// Get current <see cref="RectCorners"/>;
+    /// </summary>
+    abstract protected RectCorners cornersGet();
+
+    /// <summary>
+    /// Cache <see cref="_corners"/>;
+    /// </summary>
+    /// <remarks>
+    /// should be invoked by the derived type whenever the corners changed;
+    /// </remarks>
+    protected void cornersCache()
+    {
+      _corners = cornersGet();
+    }
+
+    #endregion
+
+    #region Protected, Fields
+
+    /// <summary>
+    /// Cache of the corners to draw;
+    /// </summary>
+    [SerializeField]
+    [HideInInspector]
+    protected RectCorners _corners;
+
+    #endregion
+  }
+}
